344ee02f52fab0116b8e49b737b92ac90f7a383e
TuxDave <godinodavide@gmail.com>
2023-08-29 11:25:26 +0200

    core configuration

---
+++ core/src/main/kotlin/com/tuxdave/mic1_simulator_kt/Hello.kt
@@ -0,0 +1,6 @@
+package com.tuxdave.mic1_simulator_kt
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ core/src/test/kotlin/com/tuxdave/mic1_simulator_kt/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.tuxdave.mic1_simulator_kt
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
